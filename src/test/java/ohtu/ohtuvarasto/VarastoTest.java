639a50657ff486d645a379151fbe72758839b306
akuivan <akuivan@users.noreply.github.com>
2020-01-25 14:43:27 +0200

    10. CircleCI: testiä muutettu niin, että se menee läpi

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -111,8 +111,7 @@ public class VarastoTest {
     @Test
     public void tulostetaanMerkkijonoOikein() {
         varasto.lisaaVarastoon(4);
-//        assertEquals("saldo = 4.0, vielä tilaa 6.0", varasto.toString());
-         assertEquals("saldo = 4.0, vielä tilaa 3.0", varasto.toString());
+        assertEquals("saldo = 4.0, vielä tilaa 6.0", varasto.toString());
     }
 //eka konstruktori
     @Test
